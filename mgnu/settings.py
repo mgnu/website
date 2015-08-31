95abad7e8a3fef3072aa2f464842cbaa9f4cad0e
Emin Mastizada <emin@mastizada.com>
2015-08-31 17:09:00 +0300

    added exceptions for not found or empty objects

--- mgnu/settings.py
+++ mgnu/settings.py
@@ -79,14 +79,6 @@ SUMMERNOTE_CONFIG = {
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 # PostgreSQL:
 # DATABASES = {    
 #     'default': {
